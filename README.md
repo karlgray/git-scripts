985387ba485dafe0caaab5e76239c83c2a4e0dc9
Karl Gray <github@gray.me.uk>
2021-06-19 18:55:03 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # git-scripts
-Bashrc commands for handling git/github 
+Bashrc commands for handling git/github.
+
+I use these in my .bashrc file to quickly run a chain of git commands and to do some basic sanity checks.
+
+My git process is that I have 2 main branches, live and staging.  Live can never be committed to or pushed to on github.
+Live can only be merged into via a PR on github.
+
+Staging can be merged locally and pushed to remote where it is automatically deployed using webhooks.  Any developer in the team can merge to staging and test.
+
+
+
+## gittest()
+
+This is used to merge active branch into staging branch directly
+
+usage: *gittest*
+
+* Checks out staging branch
+* Merges active branch, 
+* Pushes, 
+* Checkouts the active branch.
+
+
+## gitsave()
+
+Used to quickly save your work and push it to remote.
+
+usage: *gitsave "Commit message"*
+
+* Pulls the branch (to bring it up to date with remote)
+* Adds all
+* Commits with given message
+* Pushes to remote
+
---
+++ scripts.sh
@@ -0,0 +1,128 @@
+# checks out staging branch, merges active branch, pushes, then checkouts the active branch.
+gittest() {
+	branch_name="$(git symbolic-ref --short -q HEAD)"
+	git checkout staging
+	git pull
+	git merge $branch_name
+	git push
+	git checkout $branch_name
+}
+
+#  pulls the branch to bring it up to date with remote,  adds all, commits with given message the pushes to remote
+gitsave() {
+	if [ "$#" -ne 1 ]; then
+		echo "You need to provide commit message/comment"
+		return
+	fi
+
+	branch_name="$(git symbolic-ref --short -q HEAD)"
+	if [ $branch_name = "main" ]
+	then
+		echo "Cannot add, or commit to main, use branch."
+		return
+	fi
+
+	git pull
+	git add .
+	git commit -m "$1"
+	git push
+}
+
+# Checksout a branch and merges from main to bring it up to date and then pushes.
+gitcheckout() {
+	if [ "$#" -ne 1 ]; then
+		echo "You need to provide commit message/comment"
+		return
+	fi
+
+	if [ $1 = "main" ]
+	then
+		echo "Cannot checkout main,  use native git commands."
+		return
+	fi
+
+# Checkout branch
+git checkout $1
+# Catch us up to main
+git merge main
+# push to branch origin.
+git push
+# All up to date now
+}
+
+gitnewbranch() {
+	if [ "$#" -ne 1 ]; then
+		echo "You need to provide the name of the branch - All lowercase words separated by - "
+		echo "Prefix should be either feature- or hotfix-"
+		return
+	fi
+
+	git checkout main || {
+		echo "failed on git checkout of main"
+		return
+	}
+
+	git pull || {
+		echo "failed on git pull"
+		return
+	}
+
+	git checkout -b $1 || {
+		echo "failed on creation of branch"
+		return
+	}
+
+	git push --set-upstream origin $1
+}
+
+gitcleanup() {
+	branch_name="$(git symbolic-ref --short -q HEAD)"
+	if [ $branch_name = "main" ]
+	then
+		echo "Do not run this on main."
+		return
+	fi
+
+	git checkout main || {
+		echo "Failed to checkout main"
+		return
+	}
+
+	git pull || {
+		echo "failed on git pull"
+		return
+	}
+
+	git checkout staging || {
+		echo "failed on git checkout of staging"
+		return
+	}
+
+	git pull || {
+		echo "failed on git pull"
+		return
+	}
+
+	git merge main || {
+		echo "Couldn't merge main into staging"
+		return
+	}
+
+	git push || {
+		echo "failed on git push to cpstaging"
+		return
+	}
+
+	git checkout $branch_name || {
+		echo "failed on git checkout of $branch_name"
+		return
+	}
+
+	git merge main || {
+		echo "failed on git push to $branch_name"
+		return
+	}
+}
+
+
+
